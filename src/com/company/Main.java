8443e8886a7ecca4a56018d1e5ce3c6a738a15f9
Antonín <71491435+antoninvondrovic@users.noreply.github.com>
2021-09-22 20:38:03 +0200

    nesnasim abbu

---
+++ src/com/company/Main.java
@@ -0,0 +1,246 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input = "vwfljfgvgjhvqifmmb\n" +
+                "xeiaemtsisq\n" +
+                "vyaeh\n" +
+                "olobsjuvtdxnavdsqgtfvkwspqmqupjlwdwfam\n" +
+                "fco\n" +
+                "bkstgvexcmq\n" +
+                "iqiduphdrus\n" +
+                "pd\n" +
+                "svpba\n" +
+                "crctah\n" +
+                "tavktftuli\n" +
+                "qnsrykfxloet\n" +
+                "dhayrgiwrsodruwmorlthksygxw\n" +
+                "cpco\n" +
+                "cyivgymbi\n" +
+                "afcjfb\n" +
+                "uka\n" +
+                "ksmopinvxqdfn\n" +
+                "alopwlfiwlrr\n" +
+                "syccomopmoqpbjueh\n" +
+                "wxokfvrtk\n" +
+                "jqsi\n" +
+                "c\n" +
+                "eoji\n" +
+                "icbsunyvoodb\n" +
+                "bqehaoedwwlpyqndvsgn\n" +
+                "cnawerrmsetidaaa\n" +
+                "qyocpao\n" +
+                "ab\n" +
+                "tbaaachabn\n" +
+                "fxcdasmlkj\n" +
+                "jdxypbki\n" +
+                "eqgpio\n" +
+                "kfskdadvrtvw\n" +
+                "klei\n" +
+                "bye\n" +
+                "qmeqngtfv\n" +
+                "xmbvvnnafebj\n" +
+                "ocdunckbaigbwvjprn\n" +
+                "lwm\n" +
+                "lcpddlj\n" +
+                "uefsyas\n" +
+                "vptcbdgdyhfbav\n" +
+                "avbfup\n" +
+                "xhppm\n" +
+                "bnujsbmnjrwf\n" +
+                "soxlhus\n" +
+                "fxcpqcqxbawc\n" +
+                "jfxqbmyunasfqkphup\n" +
+                "hqfgaoklntcvbo\n" +
+                "ldbdotoehdkq\n" +
+                "hpguyh\n" +
+                "htosgrovovgslw\n" +
+                "yivsbvwoibxfclrerdeu\n" +
+                "wssnwiounsucjwlkvjph\n" +
+                "xiqmniuwyoxjommlltc\n" +
+                "fwbmlvu\n" +
+                "rvfhjigdyu\n" +
+                "pdxlkaypcf\n" +
+                "nf\n" +
+                "npjnidxw\n" +
+                "qkctsh\n" +
+                "mssrobatmvalvvjm\n" +
+                "glihfwbfusjjlw\n" +
+                "qxgmyji\n" +
+                "ewvcskjaxexjosgcvba\n" +
+                "ahmeupufixanrrvcklxle\n" +
+                "cvfhyfkygeyveartbp\n" +
+                "jkpcajvfocuyeaj\n" +
+                "oqjjryj\n" +
+                "omgdv\n" +
+                "ghqlkgtvxmqfvtcum\n" +
+                "ajfourufpvvahul\n" +
+                "rpjqjfiec\n" +
+                "hctmnc\n" +
+                "krajtekbc\n" +
+                "pvmg\n" +
+                "xvalnclna\n" +
+                "ncjxtsnbhla\n" +
+                "ftpmacqhevbjtodhhitgvess\n" +
+                "bxyimngsgwlwm\n" +
+                "elanaimwwakeccut\n" +
+                "hulipnwjviywh\n" +
+                "jyveyyyamvwabnvorrwaiu\n" +
+                "lgyjxrjxowgvmbwstlf\n" +
+                "uv\n" +
+                "csssvyejydij\n" +
+                "uiyoaj\n" +
+                "bynttwpwofqg\n" +
+                "adxvmbqpvhfnepp\n" +
+                "eejowt\n" +
+                "pegauivye\n" +
+                "jbhsakhketrfl\n" +
+                "mafgbhyurfs\n" +
+                "asmplfuefqvsg\n" +
+                "cyrk\n" +
+                "lvqgmjlasttbd\n" +
+                "dygvbgml\n" +
+                "opyfebhxyckkbcgr\n" +
+                "cxcppqxym\n" +
+                "easg\n" +
+                "velonbrtcridc\n" +
+                "avwbfxfodt\n" +
+                "lemcramhe\n" +
+                "cewbejlm\n" +
+                "\n" +
+                "hxcavytm\n" +
+                "couqkmnxqwogb\n" +
+                "ieorfqjbrx\n" +
+                "kaggukgdhv\n" +
+                "bsm\n" +
+                "lngmfpk\n" +
+                "hyechnkpixceu\n" +
+                "y\n" +
+                "rlvtwlbo\n" +
+                "rbufyqbvnxjonugy\n" +
+                "vuhc\n" +
+                "nfpoqbraibdqcevocoj\n" +
+                "t\n" +
+                "hrrssxtjnuchi\n" +
+                "wyddxtdpo\n" +
+                "ubuwmxsuprc\n" +
+                "bqvwswseqlfkhdencjbsl\n" +
+                "wykxkwussr\n" +
+                "xeb\n" +
+                "hdawhjjwqly\n" +
+                "ngbhu\n" +
+                "ycpmttwjpietdg\n" +
+                "ehaelfmtxvxdsvprydrmw\n" +
+                "uxkbbtjekgcwihfcvw\n" +
+                "y\n" +
+                "sdmysnrwhwlxew\n" +
+                "emogbdxvltpxa\n" +
+                "ewmikkgqdj\n" +
+                "fttyslbofns\n" +
+                "trgiljvytghsurydsh\n" +
+                "skddarigumtxl\n" +
+                "\n" +
+                "wojupdijjsy\n" +
+                "ddao\n" +
+                "xuojkdixy\n" +
+                "qorxsrg\n" +
+                "xogk\n" +
+                "kxdcrnknastnf\n" +
+                "wgadyihfolkdqnkih\n" +
+                "\n" +
+                "kobqw\n" +
+                "lmfkiydtybwsbg\n" +
+                "lqyuhucvu\n" +
+                "jajcuijusffkhouanrwfhotjuom\n" +
+                "cknlbfgam\n" +
+                "pqfnognaw\n" +
+                "egcmdnbqcst\n" +
+                "ybcopkrlsk\n" +
+                "kwgquulrdhqwhyet\n" +
+                "jlngvdkkkkrnynrso\n" +
+                "yjir\n" +
+                "vqccglmdrdwohyfssdlxpck\n" +
+                "w\n" +
+                "dqgbicoundug\n" +
+                "iqytgc\n" +
+                "rjvngwogedfiefkdxvbxu\n" +
+                "dlyjgonm\n" +
+                "vhmvqograncvvw\n" +
+                "cnonicc\n" +
+                "ypli\n" +
+                "egkarijvwf\n" +
+                "qod\n" +
+                "whfnqnnax\n" +
+                "okbkti\n" +
+                "dwbphvuysxvo\n" +
+                "h\n" +
+                "jobgw\n" +
+                "orhychvlelwvloexqg\n" +
+                "yhrpvjaboorybwxtmfjufwvcg\n" +
+                "cenovqvnfqph\n" +
+                "exvtabwmkb\n" +
+                "uxwlidqd\n" +
+                "ubmicdt\n" +
+                "ptndipnmtbrsgex\n" +
+                "qxmvs\n" +
+                "jayoxwgd\n" +
+                "iwkyoqkmrqg\n" +
+                "qgxrncg\n" +
+                "yhdamq\n" +
+                "gyvtljpp\n" +
+                "ruyawnpqkr\n" +
+                "qhcodrscj\n" +
+                "nthnchivn\n" +
+                "gduybtxbbm\n" +
+                "fyfyubmdv\n" +
+                "dnjbdmfxstqgbd\n" +
+                "bavxageyqqlvfddoj\n" +
+                "wrafmcnqwnjgiheitpulifxbka\n" +
+                "kojilqcvobhvibnpk\n" +
+                "bt\n" +
+                "oedrcjkgpt\n" +
+                "hcdqvgifqxhd\n" +
+                "yqbgalji\n" +
+                "piahhkkdonix\n" +
+                "bfmpcufmjayjkdrriq\n" +
+                "ajyoguxikvmuwspan\n" +
+                "xhjdumdqglbbsab\n" +
+                "wfjc\n" +
+                "bgauwo\n" +
+                "upjdtytpippqyf\n" +
+                "vdpgajb\n" +
+                "aallugyabfv\n" +
+                "ljgxcagbpaojgoyk";
+
+        String[] split = input.split("\n");
+
+        boolean ab = false;
+        boolean ba = false;
+
+        for (int i = 0; i < split.length; i++) {
+            for (int j = 0; j < split[i].length(); j++) {
+                char c = split[i].charAt(j);
+                char d = 0;
+                try {
+                    d = split[i].charAt(j + 1);
+                } catch (StringIndexOutOfBoundsException e) {
+
+                }
+                if ((c == 'a') && (d == 'b')) {
+                    ab = true;
+                }
+                if ((c == 'b') && (d == 'a')) {
+                    ba = true;
+                }
+
+            }
+            if (ab && ba) {
+                System.out.println((i + 1) + ". Řádka");
+                return;
+            }
+            ab = false;
+            ba = false;
+        }
+    }
+}
